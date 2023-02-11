01c95bd9355f5aa8cea2e2ccde228e6ee1305f9c
sjinseob <sjinseob@gmail.com>
2023-02-11 02:07:50 -0500

    improved 1-17.c solution

--- Chapter 1. A Tutorial Introduction/1.9 Character Arrays/1-17.c
+++ Chapter 1. A Tutorial Introduction/1.9 Character Arrays/1-17.c
@@ -1,69 +1,39 @@
-#include <stdio.h>
-
-#define OVER	80
-#define MAXLINE	1000
-
-int getLine(char s[]);
-void copy(char dest[], char orig[]);
-
-/* Note: Strings in C are actually arrays of characters */
-
 /* 
  *	Exercise 1-17:
  *	Write a program to print all input lines that are longer than 
  *	80 characters. 
  */
- 
- /* main control flow */
-int main()
-{	
- 	int i, length;
- 	char currentLine[MAXLINE];     /* current line */
- 	char longerthan80[MAXLINE];    /* line with more than 80 chars */
- 	char allLinesOver80[10];       /* All lines more than 80 */
- 	
- 	for (i = 0; i < 10; ++i) {
- 	   allLinesOver80[i] = '\0';
- 	}
- 	  
- 	while ((length = getLine(currentLine)) > 0) {
- 		if (length >= 80) {
- 		   copy(longerthan80, currentLine);
- 		   i = 0;
- 		   while (longerthan80[i] != '\0') {
- 		       putchar(longerthan80[i]);   /* write line to stdout */
- 		       ++i;
- 		   }
- 		}
-    }
-    return 0;
+#include <stdio.h>
+#define INDIV_LINE_SIZE	50000
+#define BUFFER_SIZE	50000
+
+int getLine(char s[]);
+
+int main() {	
+ 	int i = 0, l = 0;
+	char line[INDIV_LINE_SIZE];
+ 	char buffer[BUFFER_SIZE];
+
+	/* Get a line, and if EOF is not reached, keep printing out all lines that exceed length 80 to the buffer. Maximum buffer size can be set as desired.*/
+	while ((l = getLine(line)) >= 0) {
+		if (l >= 80) {
+			for (int j = 0; j < l; j++)
+				buffer[i++] = line[j];
+			buffer[i++] = '\n';
+		}
+	}
+	buffer[i] = '\0';
+
+	printf("%s\n", buffer); /* Final buffer must only contain lines exceeding 80 characters in length; print it out. */
+	return 0;
 }
- 
-/* Function that parses the current line, returns its length */
-int getLine(char line[])
-{
- 	int i, c;
- 	i = 0;
- 	while (i < MAXLINE-1 && (c = getchar()) != EOF && c != '\n') {
- 		line[i] = c;
- 		++i;
- 	}
- 	if (c == '\n') {
- 		line[i] = c;
- 		++i;
- 	}
- 	line[i] = '\0';
- 	return i;
+
+/* Gets a line, then returns its length. */
+int getLine(char s[]) {
+	int i = 0;
+	char c;
+	while ((c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if (c == EOF) return -1;
+	return i;
 }
- 
-/* helper function for copying char array */
-void copy(char to[], char from[]) {
- 	int i;
- 	i = 0;
- 	
- 	while ((to[i] = from[i]) != '\0')
- 	   ++i;
- 	return;
- }
- 
- 
